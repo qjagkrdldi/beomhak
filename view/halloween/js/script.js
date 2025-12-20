b1b86a57fae0d783cce9a871c9cd0d61557900d1
beom <qjagkrdldi@gmail.com>
2025-12-20 22:49:47 +0900

    2025.12.20 hallowin

--- view/halloween/js/script.js
+++ view/halloween/js/script.js
@@ -65,72 +65,44 @@ $(function () {
 });
 
 $(function () {
-  
+  gsap.registerPlugin(ScrollTrigger);
 
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: ".cont03",
-        start: "0% 80%",
-        end: "80% 100%",
-        scrub: 1,
-        // markers:true,
-      },
-    })
-    .to(
-      ".wrap",
-      { backgroundColor: "#fff", color: "#000", ease: "none", duration: "5" },
-      0
-    )
-    .to(".scroll", { opacity: "0", ease: "none", duration: "5" }, 0)
-    .fromTo(
-      ".videoWrap video",
-      { "clip-path": "inset(60% 60% 60% 60% round 30%)" },
-      {
-        "clip-path": "inset(0% 0% 0% 0% round 0%)",
-        ease: "none",
-        duration: 10,
-      },
-      0
-    );
-
-  gsap.timeline({
-    scrollTrigger: {
-      trigger: ".cont03",
-      start: "0% 0%",
-      pin: true,
-      scrub: 1,
-      // markers:true,
-    },
-  });
-
-  const slides = document.querySelectorAll(
-    ".cont05 .inner .swiper01 .swiper-slide"
-  );
+  const scrollEl = document.querySelector(".scroll");
 
   const tl = gsap.timeline({
     scrollTrigger: {
-      trigger: ".cont05",
-      start: "50% 80%",
-      end: "60% 30%",
+      trigger: ".cont03",
+      start: "0% 80%",
+      end: "80% 100%",
       scrub: 1,
-      // markers: true,
     },
   });
 
-  slides.forEach((slide, i) => {
-    tl.from(
-      slide,
-      {
-        x: 200,
-        opacity: 0,
-        duration: 3,
-        ease: "power2.out",
-        delay: i * 0.3, // 각 슬라이드마다 0.3초씩 추가 딜레이
-      },
-      0
-    );
-  });
+  tl.to(".wrap", {
+    backgroundColor: "#fff",
+    color: "#000",
+    ease: "none",
+    duration: 5,
+  }, 0);
+
+  if (scrollEl) {
+    tl.to(scrollEl, {
+      opacity: 0,
+      ease: "none",
+      duration: 5,
+    }, 0);
+  }
+
+  tl.fromTo(
+    ".videoWrap video",
+    { clipPath: "inset(60% 60% 60% 60% round 30%)" },
+    {
+      clipPath: "inset(0% 0% 0% 0% round 0%)",
+      ease: "none",
+      duration: 10,
+    },
+    0
+  );
 });
 
 $("h1").click(function () {
